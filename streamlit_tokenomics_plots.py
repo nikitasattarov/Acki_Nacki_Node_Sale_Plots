e527acebcc001c2d14be35df5705cc6da25097ae
nikitasattarov <Nikita09082000@gmail.com>
2024-08-27 14:22:38 +0400

    some updates

--- streamlit_tokenomics_plots.py
+++ streamlit_tokenomics_plots.py
@@ -157,12 +157,14 @@ if node_type_option == r"Block Keeper":
     #st.markdown(f"<h2 style='font-weight:bold;'>Implied 1Y Token Price ($) = {round(implied_1_y_token_price, 7)} </h2>", unsafe_allow_html=True)
     with st.expander("**IMPLIED TOKEN PRICE JFHDSJFHSD**"):
         st.write("Implied 1Y Token Price is ABCDEFGH")
-    # Создание строки с текстом и вопросиком в правом верхнем углу
-    col1, col2 = st.columns([0.9, 0.1])
+        col1, col2 = st.columns([0.9, 0.1])
     with col1:
         st.markdown(f"<h2 style='font-weight:bold;'>Implied 1Y Token Price ($) = {round(implied_1_y_token_price, 7)}</h2>", unsafe_allow_html=True)
     with col2:
-        st.markdown("[?](#)", unsafe_allow_html=True)
+        st.markdown(
+            '<span style="font-size:20px;" title="Implied 1Y Token Price is the estimated price of the token after one year.">❓</span>',
+            unsafe_allow_html=True
+        )
     plot_scale = input_plot_scale()
 
     fig, ax = plt.subplots()
