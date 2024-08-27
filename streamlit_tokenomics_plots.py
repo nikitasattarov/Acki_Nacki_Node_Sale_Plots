2e06b546f4b86d28be673dac79a4eb5f310f5c0f
nikitasattarov <Nikita09082000@gmail.com>
2024-08-27 14:38:26 +0400

    some updates

--- streamlit_tokenomics_plots.py
+++ streamlit_tokenomics_plots.py
@@ -156,9 +156,9 @@ if node_type_option == r"Block Keeper":
     implied_1_y_token_price = (node_license_price + server_monthly_cost * 12) / expected_bk_apy
     st.markdown(f"<h2 style='font-weight:bold;'>Implied 1Y Token Price ($) = {round(implied_1_y_token_price, 7)} </h2>", unsafe_allow_html=True)
     st.info(r"Implied 1Y Token Price is the total yearly expenses divided by the APY. "
-        r"If the token price exceeds this value, you will make a profit calculated as:")
-
-    st.info(r"$P = \left(\text{token price} - \text{implied token price}\right) \cdot \text{APY}$.", icon="ℹ️")
+        r"If the token price exceeds this value, you will make a profit calculated as:\n"
+        r"\n$P = \left(\text{token price} - \text{implied token price}\right) \cdot \text{APY}$.",
+        icon="ℹ️")
     plot_scale = input_plot_scale()
 
     fig, ax = plt.subplots()
