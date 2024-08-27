b13ed0cfbbd5172cb3cb0cd4438154935f1714ac
nikitasattarov <Nikita09082000@gmail.com>
2024-08-27 19:02:43 +0400

    some updates

--- streamlit_tokenomics_plots.py
+++ streamlit_tokenomics_plots.py
@@ -174,9 +174,9 @@ if node_type_option == r"Block Keeper":
     values_stake = (values_tokens - values_ff)
     min_y_value = min(list(values_tokens))
     max_y_value = max(list(values_tokens))
-    ax.plot(values_x, values_tokens, color = "Red", label = "Validator Minted Token Amount")
-    ax.plot(values_x, values_ff, color = "Blue", label = "Validator Free Float Amount")
-    ax.plot(values_x, values_stake, color = "Black", label = "Validator Staked Tokens")
+    ax.plot(values_x, values_tokens, color = "Red", label = "Validator Minted Tokens")
+    ax.plot(values_x, values_ff, color = "Blue", label = "Validator Unstaked Tokens")
+    ax.plot(values_x, values_stake, color = "Black", label = "Validator Min Staked Tokens")
     ax.set_ylim([min_y_value, max_y_value * dec(1.2)])
     y_ticks = ax.get_yticks()
     if max_y_value > 10 ** 9:
