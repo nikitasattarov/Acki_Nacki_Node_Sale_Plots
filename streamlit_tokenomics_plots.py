871d417e8ca47fad896acf61294b46970f15f2f8
nikitasattarov <Nikita09082000@gmail.com>
2024-08-27 14:43:44 +0400

    some updates

--- streamlit_tokenomics_plots.py
+++ streamlit_tokenomics_plots.py
@@ -157,7 +157,7 @@ if node_type_option == r"Block Keeper":
     st.markdown(f"<h2 style='font-weight:bold;'>Implied 1Y Token Price ($) = {round(implied_1_y_token_price, 7)} </h2>", unsafe_allow_html=True)
     st.info(r"""
     Implied 1Y Token Price is the total yearly expenses divided by the APY.     
-    If the token price exceeds this value, you will make a profit calculated as:
+    If the token price exceeds this value, you will make a profit calculated as:    
     $P = \left(\text{token price} - \text{implied token price} \right) \cdot \text{APY}$
     """, icon="ℹ️")
     plot_scale = input_plot_scale()
