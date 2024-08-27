b925c9b6af28c8bc6ef328302337cd2296d4b57c
nikitasattarov <Nikita09082000@gmail.com>
2024-08-27 13:32:19 +0400

    some tests

--- streamlit_tokenomics_plots.py
+++ streamlit_tokenomics_plots.py
@@ -164,7 +164,7 @@ if node_type_option == r"Block Keeper":
     values_ff = np.array([minted_tokens_number_calc(t, TotalSupply, KFS, u_tokens, FRC, ParticipantsNum, number_of_purchased_licenses) / FRC * dec(0.75) * free_float(t, FFF, maxFF, u_ff) for t in values_x])
     min_y_value = min(list(values_tokens))
     max_y_value = max(list(values_tokens))
-    ax.plot(values_x, values_tokens, color = "Red", label = "Minted Token Amount")
+    #ax.plot(values_x, values_tokens, color = "Red", label = "Minted Token Amount")
     ax.plot(values_x, values_ff, color = "Blue", label = "Free Float Amount")
     ax.set_ylim([min_y_value, max_y_value * dec(1.2)])
     y_ticks = ax.get_yticks()
