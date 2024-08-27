29f15f4d9f82e070bc82a2c2ad417dfcd08eae0e
nikitasattarov <Nikita09082000@gmail.com>
2024-08-27 10:55:41 +0400

    some fixes

--- streamlit_tokenomics_plots.py
+++ streamlit_tokenomics_plots.py
@@ -352,7 +352,7 @@ if node_type_option == r"Block Keeper":
     expected_bk_apy = expected_apy_calc(TotalSupply, KFS, u, SecondsInYear, FRC, BKNum)
     raised_amount = node_license_price * number_of_licenses
     implied_1_y_token_price = node_license_price / expected_bk_apy
-    st.write("Implied 1Y Token Price = ", round(implied_1_y_token_price, 7))
+    st.write("Implied 1Y Token Price = " + str(round(implied_1_y_token_price, 7)))
 
 
 if node_type_option == r"Block Manager":
@@ -405,7 +405,7 @@ if node_type_option == r"Block Manager":
     expected_bm_apy = expected_apy_calc(TotalSupply, KFS, u, SecondsInYear, FRC, BMNum)
     raised_amount = node_license_price * number_of_licenses
     implied_1_y_token_price = node_license_price / expected_bm_apy
-    st.write("Implied 1Y Token Price = ", round(implied_1_y_token_price, 7))
+    st.write("Implied 1Y Token Price = " + str(round(implied_1_y_token_price, 7)))
 
 # 1 plot
 
