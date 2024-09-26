89dc4c21207560adfbf53b932e25a65fb3948877
nikitasattarov <Nikita09082000@gmail.com>
2024-09-26 14:45:27 +0400

    some updates

--- streamlit_tokenomics_plots.py
+++ streamlit_tokenomics_plots.py
@@ -225,8 +225,8 @@ if node_type_option == r"Block Keeper":
 
 
     values_stake_pct = np.array([(dec(1) - free_float(t, FFF, maxFF, u_ff)) for t in values_x])
-    min_y_value = min(list(values_tokens))
-    max_y_value = max(list(values_tokens))
+    min_y_value = min(list(values_stake_pct))
+    max_y_value = max(list(values_stake_pct))
     ax.plot(values_x, values_stake_pct, color = "Black", label = "Block Keeper Staked Tokens (Fraction of Supply)")
     ax.set_ylabel(r'Fraction of Supply')
     ax.set_ylim([min_y_value, max_y_value * dec(1.2)])
